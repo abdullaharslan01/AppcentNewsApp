1cfd1c0d373edfbc2afc780e3e740ce9210b861d
Abdullah <125302932+abdullaharslan01@users.noreply.github.com>
2024-05-12 13:46:18 +0300

    Update ANApiUrls.swift
    an explanation added for API key

--- AppcentNewsApp/Utilities/ANApiUrls.swift
+++ AppcentNewsApp/Utilities/ANApiUrls.swift
@@ -9,5 +9,6 @@ import Foundation
 
 enum ANApiUrls{
     static let baseURL = "https://newsapi.org/v2/everything"
-    static let apiKEY  = "b4500a14a2d54d30a6f903c6e3631488"
+    // Your can add your apiKey
+    static let apiKEY  = "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
 }
